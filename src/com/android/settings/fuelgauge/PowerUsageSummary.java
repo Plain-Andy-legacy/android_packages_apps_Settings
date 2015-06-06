3d15bab545689491d418a9d522c838ee5dabbac0
Snuzzo <jrracinfan@gmail.com>
2015-06-06 12:48:53 -0400

    Overaccounted stats; merge in missing import

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -25,6 +25,7 @@ import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
 import android.os.BatteryStats;
 import android.os.Bundle;
+import android.os.Build;
 import android.os.Handler;
 import android.os.Message;
 import android.os.UserHandle;
